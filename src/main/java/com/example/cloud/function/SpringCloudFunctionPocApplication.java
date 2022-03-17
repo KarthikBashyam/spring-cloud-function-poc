63b292b88ffdaef69aa1d6303baf9590984b5e03
karthikb4ps@gmail.com <karthikb4ps@gmail.com>
2022-03-17 19:46:18 -0400

    Initial Commit

---
+++ src/main/java/com/example/cloud/function/SpringCloudFunctionPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud.function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFunctionPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFunctionPocApplication.class, args);
+	}
+
+}
